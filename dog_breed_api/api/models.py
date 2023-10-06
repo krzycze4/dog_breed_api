30dd0d9a582c20e5e463c99b6264c0c3a16fb2ab
Krzychu <czernicki.krz@gmail.com>
2023-10-06 19:56:05 +0200

    fix(models): fix bug with change fields between Dog and Breed

--- dog_breed_api/api/models.py
+++ dog_breed_api/api/models.py
@@ -15,23 +15,8 @@ class Breed(models.Model):
         (LARGE, "large"),
     ]
 
-    FEMALE = "F"
-    MALE = "M"
-
-    GENDER_CHOICES = [(FEMALE, "female"), (MALE, "male")]
-
     name = models.CharField(max_length=20)
     size = models.CharField(max_length=6, choices=SIZE_CHOICES)
-    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
-    color = models.CharField(max_length=20)
-    favourite_food = models.CharField(max_length=30)
-    favourite_toy = models.CharField(max_length=30)
-
-
-class Dog(models.Model):
-    name = models.CharField(max_length=20)
-    age = models.PositiveIntegerField()
-    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL, null=True)
     friendliness = models.PositiveIntegerField(
         default=1, validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
@@ -44,3 +29,18 @@ class Dog(models.Model):
     exercise_needs = models.PositiveIntegerField(
         default=1, validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
+
+
+class Dog(models.Model):
+    FEMALE = "F"
+    MALE = "M"
+
+    GENDER_CHOICES = [(FEMALE, "female"), (MALE, "male")]
+
+    name = models.CharField(max_length=20)
+    age = models.PositiveIntegerField()
+    breed = models.ForeignKey(Breed, on_delete=models.SET_NULL, null=True)
+    color = models.CharField(max_length=20)
+    gender = models.CharField(max_length=1, choices=GENDER_CHOICES)
+    favourite_food = models.CharField(max_length=30)
+    favourite_toy = models.CharField(max_length=30)

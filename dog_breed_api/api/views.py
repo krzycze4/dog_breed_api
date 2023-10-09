21caadf3345036504114aa1137f4103f6b47053b
Krzychu <czernicki.krz@gmail.com>
2023-10-09 13:47:57 +0200

    feat(api/views): create DogDetail and DogList Mixins and GenericAPIView

--- dog_breed_api/api/views.py
+++ dog_breed_api/api/views.py
@@ -1,6 +1,11 @@
 from django.http import Http404
 from rest_framework import status
-from rest_framework.generics import GenericAPIView
+from rest_framework.generics import (
+    GenericAPIView,
+    RetrieveUpdateDestroyAPIView,
+    ListAPIView,
+    CreateAPIView,
+)
 from rest_framework.mixins import (
     RetrieveModelMixin,
     UpdateModelMixin,
@@ -102,56 +107,79 @@ from rest_framework.views import APIView
 #         return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-# Mixins and GenericAPIView
-class DogDetail(
-    RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView
-):
+# # Mixins and GenericAPIView
+# class DogDetail(
+#     RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView
+# ):
+#     queryset = Dog.objects.all()
+#     serializer_class = DogSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+#
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+#
+#
+# class DogList(ListModelMixin, CreateModelMixin, GenericAPIView):
+#     queryset = Dog.objects.all()
+#     serializer_class = DogSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+#
+#
+# class BreedDetail(
+#     RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView
+# ):
+#     queryset = Breed.objects.all()
+#     serializer_class = BreedSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+#
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+#
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+#
+#
+# class BreedList(ListModelMixin, CreateModelMixin, GenericAPIView):
+#     queryset = Breed.objects.all()
+#     serializer_class = BreedSerializer
+#
+#     def get(self, request, *args, **kwargs):
+#         return self.list(request, *args, **kwargs)
+#
+#     def post(self, request, *args, **kwargs):
+#         return self.create(request, *args, **kwargs)
+
+
+# RetrieveUpdateDestroyAPIView, ListAPIView, CreateAPIView
+
+
+class DogDetail(RetrieveUpdateDestroyAPIView):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-class DogList(ListModelMixin, CreateModelMixin, GenericAPIView):
+class DogList(ListAPIView, CreateAPIView):
     queryset = Dog.objects.all()
     serializer_class = DogSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
 
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)
-
-
-class BreedDetail(
-    RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin, GenericAPIView
-):
+class BreedDetail(RetrieveUpdateDestroyAPIView):
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
 
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
 
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
-
-
-class BreedList(ListModelMixin, CreateModelMixin, GenericAPIView):
+class BreedList(ListAPIView, CreateAPIView):
     queryset = Breed.objects.all()
     serializer_class = BreedSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
-
-    def post(self, request, *args, **kwargs):
-        return self.create(request, *args, **kwargs)

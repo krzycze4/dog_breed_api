81b91d7d9d0987842eb3f232364f76a79ba3ac0a
Krzychu <czernicki.krz@gmail.com>
2023-10-09 11:21:08 +0200

    feat(api/urls): create endpoint 'dogs/'

--- dog_breed_api/api/urls.py
+++ dog_breed_api/api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from .views import DogDetail
+from .views import DogDetail, DogList
 
-urlpatterns = [path("dogs/<int:pk>", DogDetail.as_view(), name="get_dog")]
+urlpatterns = [
+    path("dogs/<int:pk>", DogDetail.as_view(), name="dog_view"),
+    path("dogs/", DogList.as_view(), name="dogs_view"),
+]

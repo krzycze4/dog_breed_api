bf84a03e3cf3b03fc05b6c5cfbcabb1fb4fc513a
Krzychu <czernicki.krz@gmail.com>
2023-10-09 11:08:26 +0200

    feat(api/views): create DogDetail view (APIView) with methods get, put, delete

--- dog_breed_api/api/views.py
+++ dog_breed_api/api/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.http import Http404
+from rest_framework import status
+from rest_framework.response import Response
+from .models import Dog
+from .serializers import DogSerializer
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class DogDetail(APIView):
+    def get_object(self, pk):
+        try:
+            dog = Dog.objects.get(id=pk)
+        except Dog.DoesNotExist:
+            raise Http404
+        else:
+            return dog
+
+    def get(self, request, pk):
+        dog = self.get_object(pk)
+        serializer = DogSerializer(dog)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        dog = self.get_object(pk=pk)
+        serializer = DogSerializer(dog, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        dog = self.get_object(pk=pk)
+        dog.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
